20a74c860b24b66963e79a16d866900dba32965a
Enzo <enzocastro761@gmail.com>
2021-08-18 09:42:42 -0300

    Version 1

---
+++ src/turismo/Atracciones.java
@@ -0,0 +1,19 @@
+package turismo;
+
+public class Atracciones {
+
+	private String nombre;
+	private float costo;
+	private float tiempo;
+	private int cupo;
+	private String tipo;
+
+	public Atracciones(String nombre,float costo, float tiempo, int cupo, String tipo) {
+		this.nombre = nombre;
+		this.costo = costo;
+		this.tiempo = tiempo;
+		this.cupo = cupo;
+		this.tipo = tipo;
+	}
+	
+}
